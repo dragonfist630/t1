4ddcf32796d137eb0c50b3377668b19c9be00171
Nayan Gadhari <42470897+dragonfist630@users.noreply.github.com>
2022-03-26 17:04:11 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ const server = app.listen(app.get("port"), () => {
   console.log(`API Server running at http://localhost:${app.get("port")} (${process.env.NODE_ENV})`);
 });
 
-export default server;
+export server;
